b31d496a599aad783c131ca44191602429464db4
Lucas Moraes <lucasdemoraesc@gmail.com>
2020-09-30 20:36:59 -0300

    PrimeNG Config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'uegmoney-ui';
+export class AppComponent implements OnInit {
+  title = 'Uegmoney';
+
+  constructor(private primeNGConfig: PrimeNGConfig) { }
+
+  ngOnInit() {
+    this.primeNGConfig.ripple = true; // Habilita animações ripple (Ondulações)
+  }
+
 }
