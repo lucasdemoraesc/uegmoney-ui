095431d244013b212f7f8b93b2dd505eebb5f4d2
Lucas Moraes <lucasdemoraesc@gmail.com>
2020-10-06 00:26:05 -0300

    Update wellcome page

--- src/app/components/view/wellcome/wellcome.component.ts
+++ src/app/components/view/wellcome/wellcome.component.ts
@@ -9,8 +9,9 @@ import { WellcomeService } from '../../../services/wellcome.service';
 })
 export class WellcomeComponent implements OnInit {
 
-    hour: any;
-    message: WellcomeMessage;
+  hour: any;
+  image: string;
+  message: WellcomeMessage;
 
   constructor(private wellcomeService: WellcomeService) { }
 
@@ -18,8 +19,21 @@ export class WellcomeComponent implements OnInit {
     var tmp = new Date();
     this.hour = tmp.getHours();
 
+    this.changeImage();
+
     this.wellcomeService.getMessage().subscribe(message => {
       this.message = message
     });
+  }
+
+  private changeImage() {
+    if (this.hour >= 6 && this.hour < 12)
+      this.image = "assets/img/morning.svg";
+
+    else if (this.hour >= 12 && this.hour < 18)
+      this.image = "assets/img/evening.svg";
+
+    else
+      this.image = "assets/img/night.svg";
   }
 }
